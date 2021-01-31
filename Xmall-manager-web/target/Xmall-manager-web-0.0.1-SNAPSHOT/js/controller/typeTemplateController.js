74c09b431da125b02d534de1e7bbc14cddb466bc
ziyonghong <1131002466@qq.com>
2021-01-31 17:12:00 +0800

    completed verify login and logout with Spring Security .

--- Xmall-manager-web/target/Xmall-manager-web-0.0.1-SNAPSHOT/js/controller/typeTemplateController.js
+++ Xmall-manager-web/target/Xmall-manager-web-0.0.1-SNAPSHOT/js/controller/typeTemplateController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('typeTemplateController' ,function($scope,$controller,typeTemplateService,brandService){	
+app.controller('typeTemplateController' ,function($scope,$controller,typeTemplateService,brandService,specificationService){	
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -26,7 +26,10 @@ app.controller('typeTemplateController' ,function($scope,$controller,typeTemplat
 	$scope.findOne=function(id){				
 		typeTemplateService.findOne(id).success(
 			function(response){
-				$scope.entity= response;					
+				$scope.entity= response;
+				$scope.entity.brandIds=  JSON.parse($scope.entity.brandIds);//转换品牌列表
+				$scope.entity.specIds=  JSON.parse($scope.entity.specIds);//转换规格列表
+				$scope.entity.customAttributeItems= JSON.parse($scope.entity.customAttributeItems);//转换扩展属性
 			}
 		);				
 	}
@@ -88,5 +91,25 @@ app.controller('typeTemplateController' ,function($scope,$controller,typeTemplat
 			}
 		);		
 	}
+	
+	$scope.specList={data:[]};//规格列表
+	//读取规格列表
+	$scope.findSpecList=function(){
+		specificationService.selectOptionList().success(
+			function(response){
+				$scope.specList={data:response};	
+			}
+		);		
+	}
+	
+	//新增扩展属性行
+	$scope.addTableRow=function(){	
+		$scope.entity.customAttributeItems.push({});		
+	}
+	
+	//删除扩展属性行
+	$scope.deleTableRow=function(index){			
+		$scope.entity.customAttributeItems.splice(index,1);//删除			
+	} 
     
 });	
