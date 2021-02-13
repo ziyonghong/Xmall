af5688b61f3bc09912b42079bc7529b01fd18a69
ziyonghong <1131002466@qq.com>
2021-02-13 21:28:40 +0800

    add goods entry [whether to enable specifications]

--- Xmall-sellergoods-service/src/main/java/com/zyh/sellergoods/service/impl/GoodsServiceImpl.java
+++ Xmall-sellergoods-service/src/main/java/com/zyh/sellergoods/service/impl/GoodsServiceImpl.java
@@ -78,7 +78,7 @@ public class GoodsServiceImpl implements GoodsService {
 		goodsMapper.insert(goods.getGoods());		
 		goods.getGoodsDesc().setGoodsId(goods.getGoods().getId());//设置ID
 		goodsDescMapper.insert(goods.getGoodsDesc());//插入商品扩展数据
-	
+		if("1".equals(goods.getGoods().getIsEnableSpec())){
 		for(TbItem item :goods.getItemList()){
 			//标题
 			String title= goods.getGoods().getGoodsName();
@@ -107,6 +107,42 @@ public class GoodsServiceImpl implements GoodsService {
 				item.setImage ( (String)imageList.get(0).get("url"));
 			}		
 			itemMapper.insert(item);
+		}		
+		}else{					
+			TbItem item=new TbItem();
+			item.setTitle(goods.getGoods().getGoodsName());//商品KPU+规格描述串作为SKU名称
+			item.setPrice( goods.getGoods().getPrice() );//价格			
+			item.setStatus("1");//状态
+			item.setIsDefault("1");//是否默认			
+			item.setNum(99999);//库存数量
+			item.setSpec("{}");			
+			setItemValus(goods,item);					
+			itemMapper.insert(item);
+		}	
+	}
+	
+	private void setItemValus(Goods goods,TbItem item) {
+		item.setGoodsId(goods.getGoods().getId());//商品SPU编号
+		item.setSellerId(goods.getGoods().getSellerId());//商家编号
+		item.setCategoryid(goods.getGoods().getCategory3Id());//商品分类编号（3级）
+		item.setCreateTime(new Date());//创建日期
+		item.setUpdateTime(new Date());//修改日期 
+		
+		//品牌名称
+		TbBrand brand = brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId());
+		item.setBrand(brand.getName());
+		//分类名称
+		TbItemCat itemCat = itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id());
+		item.setCategory(itemCat.getName());
+		
+		//商家名称
+		TbSeller seller = sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId());
+		item.setSeller(seller.getNickName());
+		
+		//图片地址（取spu的第一个图片）
+		List<Map> imageList = JSON.parseArray(goods.getGoodsDesc().getItemImages(), Map.class) ;
+		if(imageList.size()>0){
+			item.setImage ( (String)imageList.get(0).get("url"));
 		}		
 	}
 
