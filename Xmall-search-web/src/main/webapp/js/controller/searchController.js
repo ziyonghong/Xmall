2e3755d3ccf528c971fc3e9eec06ee920faabb77
ziyonghong <1131002466@qq.com>
2021-02-28 17:03:16 +0800

    add search module [Search page and home page docking]

--- Xmall-search-web/src/main/webapp/js/controller/searchController.js
+++ Xmall-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,4 +1,4 @@
-app.controller('searchController',function($scope,searchService){	
+app.controller('searchController',function($scope,$location,searchService){	
 	//定义搜索对象的结构
 	$scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':'','pageNo':1,'pageSize':40 ,'sortField':'','sort':''};//搜索对象
 	
@@ -109,6 +109,12 @@ app.controller('searchController',function($scope,searchService){
 			}			
 		}		
 		return false;
+	}
+	
+	//加载查询字符串
+	$scope.loadkeywords=function(){
+		$scope.searchMap.keywords=  $location.search()['keywords'];
+		$scope.search();
 	}
 	
 });
